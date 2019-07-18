7ad4d7a021f919ee8bee6e83f4f49663ecad4141
Akira Ogawa <loc_engineer@icloud.com>
2019-07-18 12:21:14 +0900

    Fixes

--- src/FindAlphanumericWords.ts
+++ src/FindAlphanumericWords.ts
@@ -6,7 +6,7 @@ export default async function(text: string): Promise<FoundResult[]> {
     let execResult: RegExpExecArray = regex.exec(text)!;
     while (execResult) {
         // tslint:disable-next-line:no-unused-expression
-        if (/[A-Za-z]/.test(execResult[0]) && /[0-9]/.test(execResult[0])){
+        if (/[A-Za-z]/.test(execResult[0]) && /[0-9]/.test(execResult[0])) {
             result.push({StartIndex: execResult.index, Value: execResult[0]});
         }
         execResult = regex.exec(text)!;
