2c0d316e97cec732bd89377492be4badd188c466
Akira Ogawa <loc_engineer@icloud.com>
2019-08-30 15:01:05 +0900

    FindLowercaseWords is fixed.

--- src/FindLowercaseWords.ts
+++ src/FindLowercaseWords.ts
@@ -2,7 +2,7 @@ import TextCheckResult from "./TextCheckResult";
 
 export default async function(text: string): Promise<TextCheckResult[]> {
     const result = new Array<TextCheckResult>();
-    const regex = RegExp("[a-Za-z]+?", "mg");
+    const regex = RegExp("\b[a-z]+?\b", "mg");
     let execResult: RegExpExecArray = regex.exec(text)!;
     while (execResult) {
         result.push({Name: "FindLowercaseWords", StartIndex: execResult.index, Value: execResult[0]});
